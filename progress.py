bfc22077ee8284dc59420e7ca5b06a7ac8f3c83d
shibbo <jtandrew@umes.edu>
2022-09-11 02:05:50 -0400

    fix branch link

--- progress.py
+++ progress.py
@@ -261,7 +261,7 @@ for key in libraries:
     lib = libraries[key]
     d, f = lib.calculateProgress()
     libprog = (d / f) * 100.0
-    progressPage.append(f"| [{key}](https://github.com/shibbo/mkdd-decomp/blob/master/docs/lib/{key}.md) | {libprog}% |\n")
+    progressPage.append(f"| [{key}](https://github.com/shibbo/mkdd-decomp/blob/main/docs/lib/{key}.md) | {libprog}% |\n")
 
 with open("docs/PROGRESS.md", "w") as w:
     w.writelines(progressPage)
